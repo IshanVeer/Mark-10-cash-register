a34c17107692dca0dab3c872f0a22f6d15f41f12
Ishan Veer <ishanpveer@gmail.com>
2021-09-30 16:56:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#bill-amount");
+const checkButton = document.querySelector("check-button");
+const cashGiven = document.querySelector("#cash-given");
+ 
+checkButton.addEventListener("click", function clickEventHandler(){
+    console.log("clicked");
+})
